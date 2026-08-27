04e826ebd2414094cb3ef7d6f949a336fd09bcfe
joebuilds80 <admin@joebuilds.com.au>
2026-08-28 08:55:15 +1000

    Add portal-client-v2.js with references to other files

---
+++ portal-client-v2.js
@@ -0,0 +1,9 @@
+dashboard-v30.js         line 331
+digital-twin-v23.js      line 213
+diagnostics-v31.js       line 223
+pathway-v10.js           line 107
+reports-v15.js           line 176
+properties-v40.js        line 476
+admin-v43.js             line 592
+profile-manager-v42.js   line 380
+auth-v8.js               lines 147, 217
